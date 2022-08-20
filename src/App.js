0396630dbb639eefd187cbc5a33b00978fa11a54
Emily Reed <emilyreed89@gmail.com>
2022-08-20 13:25:46 -0400

    styling trying to get there plus more content

--- src/App.js
+++ src/App.js
@@ -35,11 +35,9 @@ function App() {
           </SwiperSlide>
           <SwiperSlide>
           <div className="slide">
-            <div className="stackLetters">
               <h2><span className="capital">M</span><span>odel</span></h2>
               <h2><span className='capital'>V</span><span>iews</span></h2>
               <h2><span className="capital">C</span><span>ontroller</span></h2>
-            </div>
           </div>
           </SwiperSlide>
         <SwiperSlide>
@@ -52,9 +50,11 @@ function App() {
         </SwiperSlide>
         <SwiperSlide>
         <div className="slide">
-        <img src={IMAGES.modelMVC}
-         alt="snippet of mongoose code" 
-         />
+        <div className="imgContain">
+          <img src={IMAGES.modelMVC}
+           alt="snippet of mongoose code"
+           />
+        </div>
          <p>The Model contains our data and is the gatekeeper to the database. The Schema defines the structure of the document and is then mapped to a MongoDB document.</p>
         </div>
         </SwiperSlide>
@@ -72,6 +72,67 @@ function App() {
          alt="snippet of ejs code" 
          />
          <p>The Controller is the logic that controls and responds to the action the user performs from the views.</p>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+        <div className="imgContain">
+          <img src={IMAGES.mvcDemo}
+           alt="snippet of ejs code"
+           />
+        </div>
+         <p>MVC works in harmony to get the request from the User and through the controllers it can be sent to Model to fetch our data and back to the controller to Views.</p>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+        <p>Let's test this out! The User decides they want a new item on the todo list! Through the page that was rendered with ejs in Views they click on the Submit button.</p>
+        <div className="imgContain">
+          <img src={IMAGES.ejsTodo}
+           alt="snippet of ejs code"
+           />
+        </div>
+         <p>This code heads off to Routes which is sent to our friend and logic keeper the Controller.</p>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+        <img src={IMAGES.vrcMeme}
+         alt="" 
+         />
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+        <p>A lot happens here at airport traffic control. First we head off to Model where we await our todo item to be created with our Mongoose schema.</p>
+        <div className="imgContain">
+          <img src={IMAGES.controllerTodo}
+           alt="snippet of ejs code"
+           />
+        </div>
+         <p>Once its's finished we get our promise back that the data has arrived and we can now update our Views to render the new item we added.</p>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+        <p>You may still be wondering about what's going on at Model. What is a Mongoose model and this schema magic?</p>
+        <div className="imgContain">
+          <img src={IMAGES.mongooseMeme}
+           alt="snippet of ejs code"
+           />
+        </div>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+        <p>A Mongoose Model defines the interface for interacting with our DB. The model is what handles the reading, creating, updating, and deleting of documents.</p>
+        
+        <div className="imgContain">
+          <img src={IMAGES.modelMVC}
+           alt="snippet of ejs code"
+           />
+        </div>
+        <p>A schema answers the the question, "What will the data in the mongoDB collection look like?"</p>
         </div>
         </SwiperSlide>
         <SwiperSlide>
--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,12 @@ const IMAGES = {
     mToCRelay: require('./mcmeme.jpg'),
     modelMVC : require('./modelMVC1.png'),
     viewMVC : require('./viewMVC1.png'),
-    controllerMVC : require('./controllerMVC.png')
+    controllerMVC : require('./controllerMVC.png'),
+    mvcDemo : require('./mvcdemo.png'),
+    ejsTodo : require('./ejsTodo.png'),
+    controllerTodo: require('./controllerTodo.png'),
+    mongooseMeme : require('./mongoose.jpg'),
+    vrcMeme : require('./vrcMeme.jpg')
 }
 
 export default IMAGES
