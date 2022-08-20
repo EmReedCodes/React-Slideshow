85b62627984044cb0be4fe38206cf481ed7d001a
Emily Reed <emilyreed89@gmail.com>
2022-08-19 21:56:33 -0400

    adding content

--- src/App.js
+++ src/App.js
@@ -21,12 +21,73 @@ function App() {
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide><div className="img">
-        <img src={IMAGES.imageOne} alt="" />
-        </div></SwiperSlide>
-      <SwiperSlide><div className="img">Does</div></SwiperSlide>
-      <SwiperSlide><div className="img">This</div></SwiperSlide>
-      <SwiperSlide><div className="img">Work</div></SwiperSlide>
+      
+      <SwiperSlide>
+        <div className="slide">
+        <h3>What the heck is MVC?</h3>
+        <img src="https://media.giphy.com/media/3o6MbbwX2g2GA4MUus/giphy.gif" 
+        alt="Homer Simpson frantically searching through book gif" 
+        />
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+          <h2><span className="capital">M</span><span>odel</span></h2>
+          <h2><span className='capital'>V</span><span>iews</span></h2>
+          <h2><span className="capital">C</span><span>ontroller</span></h2>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+          <p>Let's do a little throw back! Remember a little something called seperation of concern? The idea that each module or layer in an application should only be responsible for one thing. </p>
+          <img src="https://media.giphy.com/media/xT5LMXD12I3SdzfRcI/giphy.gif" 
+          alt="Simpson character Millhouse's mom knocking on door" srcset="" />
+          <p>Well MVC is based on a similar concept. An architecture designed to keep our back end more readable and easy to change.</p>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+        <img src={IMAGES.modelMVC}
+         alt="snippet of mongoose code" 
+         />
+         <p>The Model contains our data and is the gatekeeper to the database. The Schema defines the structure of the document and is then mapped to a MongoDB document.</p>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+        <img src={IMAGES.viewMVC}
+         alt="snippet of ejs code" 
+         />
+         <p>The View is the part that that will get compiled using a templating engine for the user to see on the browser. We can update data in Views using the Model.</p>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+        <img src={IMAGES.controllerMVC}
+         alt="snippet of ejs code" 
+         />
+         <p>The Controller is the logic that controls and responds to the action the user performs from the views.</p>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slide">
+        <img src={IMAGES.imageOne}
+         alt="" 
+         />
+        </div>
+        </SwiperSlide>
+      <SwiperSlide>
+        <div className="slide">Does
+        </div>
+        </SwiperSlide>
+      <SwiperSlide>
+        <div className="slide">This
+        </div>
+        </SwiperSlide>
+      <SwiperSlide>
+        <div className="slide">Work
+        </div>
+        </SwiperSlide>
 
     </Swiper>
   );
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,8 @@
 const IMAGES = {
-    imageOne: require('./mcmeme.jpg')
+    mToCRelay: require('./mcmeme.jpg'),
+    modelMVC : require('./modelMVC1.png'),
+    viewMVC : require('./viewMVC1.png'),
+    controllerMVC : require('./controllerMVC.png')
 }
 
 export default IMAGES
